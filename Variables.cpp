027d60d57bbb364d63ef4ab990ab95e93d22f4c5
Abhineet <abhineet.am13@gmail.com>
2026-02-03 21:28:16 +0530

    add: create Variables.cpp with examples of variable declaration and modification

---
+++ Variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;  // Declaring an integer variable named x
+    x = 42; // Assigning the value 42 to the variable x
+    cout << x << endl; // Printing the value of x
+    x = 45; // Updating the value of x to 45
+    cout << x << endl; // Printing the updated value of x
+    x = x + 10; // Modifying the value of x by adding 10 to it
+    cout << x << endl; // Printing the modified value of x
+}
