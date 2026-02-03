bf898ee09de6b0a939e8fd55b9fcc305f215ba33
Abhineet <abhineet.am13@gmail.com>
2026-02-03 22:31:21 +0530

    add: C++ program to find remainder using modulus operator

---
+++ findRemainder.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int dividend, divisor, remainder;
+    cout << "Enter dividend: ";
+    cin >> dividend;
+    cout << "Enter divisor: ";
+    cin >> divisor;
+    remainder = dividend % divisor;
+    cout << "Remainder is: " << remainder << endl;
+}
