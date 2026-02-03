37b19339938746f96e3e95b440502c0c6c4ad42f
Abhineet <abhineet.am13@gmail.com>
2026-02-03 22:46:26 +0530

    add: implement Area of Circle calculation program in AreaOfCircle.cpp

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float radius, area;
+    const float PI = 3.14159;
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+    area = PI * radius * radius;
+    cout << "The area of the circle is: " << area << endl;
+    return 0;
+}
