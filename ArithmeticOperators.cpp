df56c1234a5c56e74fef06691fe68f739b6a8eec
Abhineet <abhineet.am13@gmail.com>
2026-02-03 21:53:54 +0530

    add: create ArithmeticOperators.cpp with examples of basic arithmetic operations

---
+++ ArithmeticOperators.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 5;
+    int y = 3;
+    
+    cout << "x + y = " << x + y << endl; // Addition
+    cout << "x - y = " << x - y << endl; // Subtraction
+    cout << "x * y = " << x * y << endl; // Multiplication
+    cout << "x / y = " << x / y << endl; // Division (
+    cout << "x % y = " << x % y << endl; // Modulus (remainder)
+
+}
