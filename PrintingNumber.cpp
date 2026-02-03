a27770f1450ebce3de512c8dfebc2ef5195ea7d1
Abhineet <abhineet.am13@gmail.com>
2026-02-03 21:17:02 +0530

    add: create PrintingNumber.cpp with examples of outputting different data types

---
+++ PrintingNumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    cout << 42 << endl; // Printing an integer number
+    cout << 3.14 << endl; // Printing a floating-point number
+    cout << 'A' << endl; // Printing a character
+    cout << "C++ Programming" << endl; // Printing a string literal
+    cout <<4+3 << endl; // Printing the result of an expression
+    cout << 4+3; 
+return 0;
+}
