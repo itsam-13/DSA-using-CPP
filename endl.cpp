81f271f6677c7ac95432bf96baee5c241297f1c4
Abhineet <abhineet.am13@gmail.com>
2026-02-03 21:11:11 +0530

    update: Explain usage of endl in C++

--- endl.cpp
+++ endl.cpp
@@ -5,6 +5,7 @@ int main()
 {
     cout << "Hello, World!" << endl;
     cout << "Welcome to C++ programming!" << endl;
+    // Using endl to insert a new line and flush the output buffer
 
 return 0;
 }
