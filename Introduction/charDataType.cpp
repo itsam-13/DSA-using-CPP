9ee1cf2f0259a40a37c1ea06728aa517ea8ea15e
Abhineet <abhineet.am13@gmail.com>
2026-02-04 21:58:06 +0530

    add: implement character input and output program in charDataType.cpp

---
+++ Introduction/charDataType.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout << " Enter a character: ";
+    cin >> ch; // Input a character from user
+    cout << " You entered: " << ch << endl;
+}
