3ca325b9777772e8b01d4c39a754a86e1235a447
itsam-13 <abhineet.am13@gmail.com>
2026-02-05 21:49:56 +0530

    add: implement program to find the greatest of three numbers in GreatestOf3Num.cpp

---
+++ Loop/GreatestOf3Num.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, num3;
+    cout<<"Enter three numbers: ";
+    cin>>num1>>num2>>num3;
+    int greatest;
+    if(num1 >= num2 && num1 >= num3)
+    {
+        greatest = num1;
+    }
+    else if(num2 >= num1 && num2 >= num3)
+    {
+        greatest = num2;
+    }
+    else
+    {
+        greatest = num3;
+    }
+    cout<<"The greatest number is: "<<greatest<<endl;
+    
+}
