06c1e44ce01e44e6d3750479a4864b1e2b184d8c
itsam-13 <abhineet.am13@gmail.com>
2026-02-08 22:47:31 +0530

    add: implement program to print odd numbers up to a specified value in AP1.cpp

---
+++ Loops/AP1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number of times you want to print series: ";
+    cin>>n;
+    for(int i=1; i<=2*n-1; i+=2)
+    {
+        cout<<i<<" ";
+    }
+
+    
+}
