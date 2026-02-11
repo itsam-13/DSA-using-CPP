49262b9fd0f588c232498f1c92d449bb5c100a59
itsam-13 <abhineet.am13@gmail.com>
2026-02-11 21:39:35 +0530

    fix: Special case for 0, which has 1 digit

--- Loops/countDigit.cpp
+++ Loops/countDigit.cpp
@@ -6,10 +6,15 @@ int main()
     cout << "Enter a number: ";
     cin >> n;
     int count = 0;
+    int a = n;
     while(n > 0)
     {
         n = n / 10;
         count++;
+    }
+    if(a==0) // Special case for 0, which has 1 digit
+    {
+        count = 1;
     }
     cout << "Number of digits: " << count << endl;
     return 0;
