ee3fbeb5788351d8a5dcef47c64fab5f21252289
itsam-13 <abhineet.am13@gmail.com>
2026-02-11 22:03:38 +0530

    add: compute alternating sum pattern result based on even or odd input number

---
+++ Loops/SumOfAlterNum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout << "Enter a number : ";
+cin>>n;
+int sum = 0;
+
+if(n%2 == 0) sum =- n/2;
+else sum = -n/2 + n;
+cout << sum;
+}
